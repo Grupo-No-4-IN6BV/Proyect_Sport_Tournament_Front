c6aaa5eeb3c4b5ab06edc81563feef307337fea6
André Ortega <aortega-2019275@kinal.edu.gt>
2021-06-13 22:33:30 -0600

    directiva delete

--- src/app/directive/changes-lr.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChangesLRDirective } from './changes-lr.directive';
-
-describe('ChangesLRDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ChangesLRDirective();
-    expect(directive).toBeTruthy();
-  });
-});
