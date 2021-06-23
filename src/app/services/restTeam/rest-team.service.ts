4240a0f311637d4d4136bf247bbefb0784460682
Spencer Ortiz <sortiz-2019191@kinal.edu.gt>
2021-06-23 00:45:03 -0600

    Update y Remove de Teams

--- src/app/services/restTeam/rest-team.service.ts
+++ src/app/services/restTeam/rest-team.service.ts
@@ -12,6 +12,7 @@ export class RestTeamService {
   public user;
   public token;
   public league;
+  public team;
   public uri: string;
   public httpOptionsAuth = {
     headers: new HttpHeaders({
@@ -36,8 +37,6 @@ export class RestTeamService {
     }
     return this.token;
   }
-   
-
 
   saveTeam(idUser,idLeague, team){
     let headers = new HttpHeaders({
@@ -48,4 +47,34 @@ export class RestTeamService {
     return this.http.put(this.uri+idUser+'/'+idLeague+'/setTeam', params,  {headers: headers})
     .pipe(map(this.extractData))
   }
+
+  removeTeam(idLeague, idTeam){
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.getToken()
+    });
+    return this.http.put(this.uri+idLeague+'/removeTeam/'+idTeam, null, {headers: headers})
+    .pipe(map(this.extractData))
+  }
+
+  updateTeam(idLeague, team){
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.getToken()
+    });
+    let params = JSON.stringify(team);
+    return this.http.put(this.uri+idLeague+'/updateTeam/'+team.id, params, {headers: headers})
+    .pipe(map(this.extractData))
+  }
+
+  getTeam(){
+    let team = JSON.parse(localStorage.getItem('team'));
+    if(team != null || team != undefined){
+      this.team = team;
+    }else{
+      this.team = null;
+    }
+    return this.team;     
+ }
+ 
 }
